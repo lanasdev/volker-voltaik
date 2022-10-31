08afb4b5cf6a793a3c355c9103b6bad719c71eed
DerMatte <me@dermatte.de>
2022-10-31 17:15:18 +0100

    codemod next-image-experimental

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/legacy/image";
+import Image from "next/image";
 import Layout from "components/Layout";
 // import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 import HeroSection from "components/HeroSection";
