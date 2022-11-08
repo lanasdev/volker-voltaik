7f9004e89c7cfb7aa5d9f2ce6dbd2daa21f6fb13
DerMatte <me@dermatte.de>
2022-11-08 11:28:46 +0100

    active nav item is now shown

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,32 +12,31 @@ import { withRouter } from "next/router";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
-  { name: "Leistungen", href: "/#leistungen", current: false },
-  { name: "Projekte", href: "/projekte", current: false },
-  { name: "Über uns", href: "/ueber", current: false },
+  {
+    name: "Leistungen",
+    href: "/#leistungen",
+    href2: "/leistungen",
+    current: false,
+  },
+  { name: "Projekte", href: "/projekte", href2: "/projekte", current: false },
+  { name: "Über uns", href: "/ueber", href2: "/ueber", current: false },
 ];
 
 const Navbar = ({ router }) => {
   let path = router.pathname;
+  console.log(path);
+
+  // get first path segment
+  path = "/" + path.split("/")[1];
+  console.log(path);
 
   return (
     <Disclosure as="nav" className="text-black">
       {({ open }) => (
         <>
-          <div className="mx-auto py-2 px-2 sm:px-6 lg:px-16">
+          <div className="mx-auto py-2 px-4 sm:px-6 lg:px-16">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-black  hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <X className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <List className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-1 items-center justify-start sm:items-stretch sm:justify-start">
                 <Link href="/" className="flex flex-shrink-0 items-center">
                   <Image
                     className="block h-10 w-auto lg:hidden"
@@ -59,7 +58,7 @@ const Navbar = ({ router }) => {
                         key={item.name}
                         href={item.href}
                         className={cn(
-                          item.href == path
+                          item.href == path || item.href2 == path
                             ? "underline decoration-yellow decoration-2 underline-offset-2 hover:bg-black/10"
                             : "decoration-2 underline-offset-2 hover:bg-black/10 hover:underline hover:decoration-yellow",
                           "rounded-lg px-[20px] py-[10px] font-medium text-black"
@@ -72,32 +71,56 @@ const Navbar = ({ router }) => {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="absolute inset-y-0 right-0 hidden items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 md:flex">
                 <CallToActionNav />
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-black  hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <X className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <List className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
               </div>
             </div>
           </div>
+
           {/* Mobile Menu */}
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-2 px-2 pt-2 pb-3">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={cn(
-                    item.href == path
-                      ? "bg-black text-white "
-                      : "hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium transition-all duration-75 hover:bg-yellow"
-                  )}
-                  aria-current={item.href == path ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Disclosure.Panel className="sm:hidden">
+              <div className="space-y-2 px-2 pt-2 pb-3">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    //  if href or href2 is equal to path, then add underline
+                    className={cn(
+                      item.href == path || item.href2 == path
+                        ? "bg-black text-white "
+                        : "hover:text-white",
+                      "block rounded-md px-3 py-2 text-base font-medium transition-all duration-75 hover:bg-yellow"
+                    )}
+                    aria-current={item.href == path ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </Transition>
         </>
       )}
     </Disclosure>
