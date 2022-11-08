4ba225f00920b34e8d03ed462032b6011dc2d6a7
DerMatte <me@dermatte.de>
2022-11-08 10:41:43 +0100

    simple analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { Fragment } from "react";
+import Script from "next/script";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Fragment>
       <Component {...pageProps} />
-    </>
+      <Script src="https://sa.lanas.dev/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://sa.lanas.dev/noscript.gi"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+    </Fragment>
   );
 }
 
